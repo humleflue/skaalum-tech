d5270ee5900772a7ba39045e3a001a7e8e339f19
Lasse Skaalum <lskaal19@student.aau.dk>
2020-09-05 17:31:47 +0200

    testing

--- app.js
+++ app.js
@@ -5,22 +5,21 @@ const express    = require(`express`);       // For running the server
 const app        = express();                // For running the server
 const port       = 3000;                     // Server port number
 const bodyParser = require(`body-parser`);   // For parsing the request. Makes all request data available in req.body
-const fs         = require(`fs`);            // For reading/writing files
 const path       = require(`path`);          // Used to avoid errors when reffering to a path in the file system
 const cors       = require(`cors`);          // Used to enable CORS
 const morgan     = require(`morgan`);        // Used to log all info about client from the request
 const favicon    = require(`serve-favicon`); // Used to serve the favicon more effectively (no need to put it in the html)
+require(`express-async-errors`);             // With this we don't have to pass errors like next(err) but can just throw'em instead
 
 /* Internal modules */
-const handleRoutes     = require(`./server/routing`);
-const handleErrors     = require(`./server/error_handler`);
-const mw               = require(`./server/middleware`);
-const consoleLogToFile = require(`./server/helpers/consol_log_file`);
+const handleRoutes         = require(`./server/routing`);
+const handleErrors         = require(`./server/error_handler`);
+const mw                   = require(`./server/middleware`);
+const consoleLogToFile     = require(`./server/helpers/consol_log_file`);
 
 /* Setup */
+global.conf = require(`./server_settings`); // Load settings into grobal variable (available across all scripts)
 consoleLogToFile(); // Modifies the console, such that it writes logs into the server/logs dir
-// Load settings into grobal variable (available across all scripts)
-global.conf = JSON.parse(fs.readFileSync(path.join(__dirname, `server_settings.json`)));
 
 /* Middleware */
 app.use(mw.requestValidator);
@@ -40,4 +39,4 @@ handleRoutes(express, app);
 handleErrors(express, app);
 app.use((req, res) => res.sendStatus(404)); // Handles non-existing URL-requests. Has to be the last line before app.listen.
 
-app.listen(port, () => console.log(`skaalum-tech listening at http://localhost:${port}`));
+app.listen(port, () => console.log(`skaalum.tech listening at http://localhost:${port}`));
